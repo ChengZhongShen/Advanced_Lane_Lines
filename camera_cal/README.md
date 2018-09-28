ad40785782e27be456493c5ca2e711746e05548d
ChengZhongShen <chengzhong.shen@gmail.com>
2018-09-28 12:53:32 +0800

    camera cal done

---
+++ camera_cal/README.md
@@ -0,0 +1,17 @@
+### Calibration the Camera
+
+Using the chessboard images to calibrate the camera.
+the calibration parameter will install into a pickle file for further use.
+
+![dist_example](dist_example.png)
+
+#### Note:
+the nx is the inner pionts in x direction
+the ny is the inner points in y direction
+
+#### Folders
+* **chessboard_img**: the original chessboard_img
+* **corners_founded**: the chessboard_img which find the correct corner numbers and drawed the corner.
+
+#### Files
+* **camera_balibration** the python scrip which used to calibration the camera.
--- camera_cal/camera_calibration.py
+++ camera_cal/camera_calibration.py
@@ -1,18 +1,24 @@
+# python 3.x
+
 import numpy as np 
 import cv2
 import glob
 import matplotlib.pyplot as plt 
 
+# !!! IMPORTANT, set the nx, ny according the calibration chessboard pictures.
+nx = 9
+ny = 6
+
 # prepare object points, like (0,0,0), (1,0,0), (2,0,0), ...(6,5,0)
-objp = np.zeros((6*8,3), np.float32)
-objp[:,:2] = np.mgrid[0:8, 0:6].T.reshape(-1,2)
+objp = np.zeros((nx*ny,3), np.float32)
+objp[:,:2] = np.mgrid[0:nx, 0:ny].T.reshape(-1,2)
 
 # Arrays to store object points and image points from all the images.
 objpoints = [] # 3d points in real world space
 imgpoints = [] # 2d pionts in image plane.
 
 # Make a list of calibration images
-images = glob.glob('calibration_wide/GO*.jpg')
+images = glob.glob('chessboard_img/calibration*.jpg')
 
 # Step through the list and search for chessboard corners
 for idx, fname in enumerate(images):
@@ -20,7 +26,7 @@ for idx, fname in enumerate(images):
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	# Find the chessboard corners
-	ret, corners = cv2.findChessboardCorners(gray, (8,6), None)
+	ret, corners = cv2.findChessboardCorners(gray, (nx,ny), None)
 
 	# If found, add object points, image points
 	if ret == True:
@@ -28,30 +34,30 @@ for idx, fname in enumerate(images):
 		imgpoints.append(corners)
 
 		# Draw and display the corners
-		# cv2.drawChessboardCorners(img, (8,6), corners, ret)
-		# write_name = 'calibration_wide/corners_found'+str(idx)+'.jpg'
-		# cv2.imwrite(write_name, img)
-		# cv2.imshow('img', img)
-		# cv2.waitKey(500)
+# 		cv2.drawChessboardCorners(img, (nx,ny), corners, ret)
+# 		write_name = 'corners_found'+str(idx)+'.jpg'
+# 		cv2.imwrite(write_name, img)
+# 		cv2.imshow('img', img)
+# 		cv2.waitKey(500)
 # cv2.destroyAllWindows()
 
 import pickle
 
 # Test undistorion on an image
-img = cv2.imread('calibration_wide/test_image.jpg')
+img = cv2.imread('chessboard_img/calibration1.jpg')
 img_size = (img.shape[1],img.shape[0])
 
 # Do camera calibration given object points and image points
 ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, img_size, None, None)
 
 dst = cv2.undistort(img, mtx, dist, None, mtx)
-cv2.imwrite('calibration_wide/test_undist.jpg', dst)
+cv2.imwrite('test_undist.jpg', dst)
 
 # Save the camera calibration result for later use (we won't worry about rvecs / tvecs)
 dist_pickle = {}
 dist_pickle["mtx"] = mtx
 dist_pickle["dist"] = dist 
-pickle.dump(dist_pickle, open("calibration_wide/wide_dist_pickle.p", "wb"))
+pickle.dump(dist_pickle, open("camera_cal.p", "wb"))
 # dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
 # Visualize undistortion
 f, (ax1,ax2) = plt.subplots(1,2, figsize=(20,10))
